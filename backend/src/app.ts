80e13ec175fccdfc650fd49e148d6cb4411d90da
Denis Kotelev <hardsmile98@mail.ru>
2022-03-12 12:47:27 +0300

    add user controller and route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,14 +2,14 @@ import express, { Application } from 'express'
 import mongoose, { ConnectOptions } from 'mongoose'
 import cors from 'cors'
 import * as dotenv from 'dotenv'
-//import users from './routes/users'
+import users from './routes/users'
 
 const app: Application = express()
 dotenv.config()
 app.use(cors())
 app.use(express.json())
 
-//app.use('/api/v1/users', users)
+app.use('/api/v1/users', users)
 
 const PORT = process.env.PORT || '5000'
 const URL = process.env.MONGODB_URL || ''
---
+++ backend/src/controllers/UserController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express'
+
+class UserController {
+  async register(req: Request, res: Response) {
+    try {
+    } catch (error) {}
+  }
+
+  async login(req: Request, res: Response) {
+    try {
+    } catch (error) {}
+  }
+}
+export default new UserController()
