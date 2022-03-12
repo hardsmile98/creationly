dc91df588ca04bcdd3bb03165c478e01733ae590
Denis Kotelev <hardsmile98@mail.ru>
2022-03-12 11:52:37 +0300

    connect to db

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,32 @@
-import express from 'express'
-const app = express()
-const PORT = 8000
-app.get('/', (req, res) => res.send('Express + TypeScript Server'))
-app.listen(PORT, () => {
-  console.log(`[server]: Server is running at https://localhost:${PORT}`)
-})
+import express, { Application } from 'express'
+import mongoose, { ConnectOptions } from 'mongoose'
+import cors from 'cors'
+import * as dotenv from 'dotenv'
+//import users from './routes/users'
+
+const app: Application = express()
+dotenv.config()
+app.use(cors())
+app.use(express.json())
+
+//app.use('/api/v1/users', users)
+
+const PORT = process.env.PORT || '5000'
+const URL = process.env.MONGODB_URL || ''
+
+async function start() {
+  try {
+    await mongoose.connect(URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as ConnectOptions)
+    app.listen(PORT, () => {
+      console.log(`server start http://localhost:${PORT}`)
+    })
+  } catch (error) {
+    console.log('server error', error)
+    process.exit(1)
+  }
+}
+
+start()
